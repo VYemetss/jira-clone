a97801ef1fe14bc07cffff35d66158c67a8c9cdf
VYemetss <viktoriayemets123@gmail.com>
2024-12-03 02:08:34 +0200

    changed styles for all basic buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div>
-      <Button variant={'destructive'} size={'lg'}>Click me</Button>
+    return (
+        <div>
+            <Button variant='primary' size={'lg'}>Click me</Button>
+            <Button variant='secondary' size={'lg'}>Click me</Button>
+            <Button variant='destructive' size={'lg'}>Click me</Button>
+            <Button variant='muted' size={'lg'}>Click me</Button>
+            <Button variant='ghost' size={'lg'}>Click me</Button>
+            <Button variant='outline' size={'lg'}>Click me</Button>
+            <Button variant='teritary' size={'lg'}>Click me</Button>
     </div>
   );
 }
