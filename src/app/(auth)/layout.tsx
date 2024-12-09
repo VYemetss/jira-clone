6deb94593bc0cbf7a4ef507bb81a4c1b761d5fdf
VYemetss <viktoriayemets123@gmail.com>
2024-12-09 22:14:28 +0200

    added a navbar and a sign up button

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
 interface AuthLayoutProps {
     children: React.ReactNode;
 }
 
-const AuthLayout = ( {children}: AuthLayoutProps ) => {
+const AuthLayout = ({children}: AuthLayoutProps) => {
     return (
-    <div>
-        <Image src='/logo.svg' height={50} width={100} alt={'logo'}/>
-        {children}
-    </div>
+        <main className={'bg-neutral-100 min-h-screen'}>
+            <div className={'mx-auto max-w-screen-2xl p-4'}>
+                <nav className={'flex justify-between items-center'}>
+                    <Image src='/logo.svg' height={56} width={152} alt={'logo'}/>
+                    <Button variant={'secondary'}>Sign Up</Button>
+                </nav>
+                {children}
+            </div>
+        </main>
     );
 };
 
