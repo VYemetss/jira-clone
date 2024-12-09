bbd2725707d9cbb62dc70bfd28e533ca384c3bf1
VYemetss <viktoriayemets123@gmail.com>
2024-12-09 22:02:33 +0200

    added a logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 interface AuthLayoutProps {
     children: React.ReactNode;
 }
 
 const AuthLayout = ( {children}: AuthLayoutProps ) => {
     return (
-    <div>{children}</div>
+    <div>
+        <Image src='/logo.svg' height={50} width={100} alt={'logo'}/>
+        {children}
+    </div>
     );
 };
 
