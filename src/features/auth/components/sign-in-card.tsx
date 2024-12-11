46bfa01835698de3f0da68c7fc1f4cc5d9136af8
VYemetss <viktoriayemets123@gmail.com>
2024-12-11 23:18:21 +0200

    sign in buttons for a sign in card

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -4,6 +4,8 @@ import {
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { DottedSeparator } from "@/components/dotted-separator";
 
 export const SignInCard = () => {
@@ -12,9 +14,44 @@ export const SignInCard = () => {
             <CardHeader className={'flex items-center justify-center text-center p-7'}>
                 <CardTitle className={'text-2xl'}>Welcome Back!</CardTitle>
             </CardHeader>
-            <div className={'px-7 mb-2'}>
+            <div className={'px-7'}>
                 <DottedSeparator />
             </div>
+            <CardContent className={'p-7'}>
+                <form className={'space-y-4'}>
+                    <Input
+                        required
+                        type={'email'}
+                        value={''}
+                        onChange={() => {}}
+                        placeholder={'Enter email address'}
+                        disabled={false}
+                    />
+                    <Input
+                        required
+                        type={'password'}
+                        value={''}
+                        onChange={() => {}}
+                        placeholder={'Enter password'}
+                        disabled={false}
+                        min={8}
+                        max={256}
+
+                    />
+                    <Button disabled={false} size={'lg'} className={'w-full'}>Log in</Button>
+                </form>
+            </CardContent>
+            <div className={'px-7'}>
+                <DottedSeparator></DottedSeparator>
+            </div>
+            <CardContent className={'p-7 flex flex-col gap-y-4'}>
+                <Button disabled={false} variant={'secondary'} size={'lg'} className={'w-full'}>
+                    Login with Google
+                </Button>
+                <Button disabled={false} variant={'secondary'} size={'lg'} className={'w-full'}>
+                    Login with Github
+                </Button>
+            </CardContent>
         </Card>
     );
 };
