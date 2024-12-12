2e8357b0f420fce615f49e264151495cbe339bb6
VYemetss <viktoriayemets123@gmail.com>
2024-12-12 22:34:28 +0200

    added a sign up card

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+
+import {SignUpCard} from "@/features/auth/components/sign-up-card";
 const SignUpPage= () => {
     return (
         <div>
-            Sign Up Page
+           <SignUpCard></SignUpCard>
         </div>
     );
 };
